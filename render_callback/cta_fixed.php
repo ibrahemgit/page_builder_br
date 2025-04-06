52ba042d479ea65c1995fcc6690b105863e238fd
ibrahemgit <seoplusade@gmail.com>
2025-04-06 12:45:44 +0200

    Update cta_fixed.php

--- render_callback/cta_fixed.php
+++ render_callback/cta_fixed.php
@@ -1,8 +1,16 @@
 <?php 
 
 function render_footer_cta_block($attributes) {
-    $phone_n = esc_attr($attributes['phoneNumber']);
-    $whats_n = esc_attr($attributes['whatsNumber']);
+    $phone_n = get_option('custom_phone');
+    $whats_n = get_option('custom_whatsapp');
+
+    if (empty($phone_n)) {
+        $phone_n = esc_attr($attributes['phoneNumber']);
+    }
+
+    if (empty($whats_n)) {
+        $whats_n = esc_attr($attributes['whatsNumber']);
+    }
 
     $post_id = get_the_ID();
     $post_title = $post_id ? get_the_title($post_id) : 'المعرض';
@@ -10,12 +18,10 @@ function render_footer_cta_block($attributes) {
     ob_start();
     ?>
     <div class="footer-cta">
-
-
-        <a id="cta_whats" target="_blank" class="social-item whats" href="https://wa.me/<?php echo $whats_n; ?>?text= اريد الاستفسار عن : <?php echo esc_html($post_title); ?>">
+        <a id="cta_whats" target="_blank" class="social-item whats" href="https://wa.me/<?php echo esc_attr($whats_n); ?>?text= اريد الاستفسار عن : <?php echo esc_html($post_title); ?>">
             <i class="fa fa-whatsapp" aria-hidden="true"></i> 
         </a>
-        <a id="cta_call" class="social-item phone" href="tel:<?php echo $phone_n; ?>">
+        <a id="cta_call" class="social-item phone" href="tel:<?php echo esc_attr($phone_n); ?>">
             <i class="fa fa-phone" aria-hidden="true"></i> 
         </a>
     </div>
