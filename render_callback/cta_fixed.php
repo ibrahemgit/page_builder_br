46cc7f2458923df77bd6c730cc3d452e92871cab
ibrahemgit <seoplusade@gmail.com>
2025-06-24 21:08:59 +0300

    Update cta_fixed.php

--- render_callback/cta_fixed.php
+++ render_callback/cta_fixed.php
@@ -24,7 +24,7 @@ function render_footer_cta_block($attributes) {
     ob_start();
     ?>
     <div class="footer-cta">
-        <a id="cta_whats" target="_blank" class="social-item whats" href="https://wa.me/+2<?php echo esc_attr($whats_n); ?>?text= اريد الاستفسار عن : <?php echo esc_html($post_title); ?>">
+        <a id="cta_whats" target="_blank" class="social-item whats" href="https://wa.me/<?php echo esc_attr($whats_n); ?>?text= اريد الاستفسار عن : <?php echo esc_html($post_title); ?>">
             <i class="fa fa-whatsapp" aria-hidden="true"></i> 
         </a>
         <a id="cta_call" class="social-item phone" href="tel:<?php echo esc_attr($phone_n); ?>">
