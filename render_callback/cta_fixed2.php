3def786d0e8c8f0dc843cdd0cd48e305d133a0d3
ibrahemgit <seoplusade@gmail.com>
2025-06-24 20:48:36 +0300

    Update cta_fixed2.php

--- render_callback/cta_fixed2.php
+++ render_callback/cta_fixed2.php
@@ -45,7 +45,7 @@ function footer_cta_block_render2($attributes) {
                     <span><?php echo $titleText; ?></span>
                 </div>
                 <div class="towitem">
-                    <a id="cta_whats" target="_blank" class="whatsapp" href="https://wa.me/<?php echo esc_attr($whatsNumber); ?>?text=أرغب في معرفة المزيد عن <?php echo esc_html($post_title); ?>">
+                    <a id="cta_whats" target="_blank" class="whatsapp" href="https://wa.me/+2<?php echo esc_attr($whatsNumber); ?>?text=أرغب في معرفة المزيد عن <?php echo esc_html($post_title); ?>">
                         <i class="fa fa-whatsapp" aria-hidden="true"></i> واتساب
                     </a>
                     <span id="subform" class='subform'>
